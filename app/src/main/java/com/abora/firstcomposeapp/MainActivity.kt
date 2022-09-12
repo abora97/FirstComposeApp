ef6679a1a2486fb71524be4cfb9a1950a9bfd33d
Abora <mahmod.elebiary@gmail.com>
2022-09-13 01:36:02 +0200

    Modifiers

--- app/src/main/java/com/abora/firstcomposeapp/MainActivity.kt
+++ app/src/main/java/com/abora/firstcomposeapp/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.abora.firstcomposeapp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -13,6 +17,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -50,20 +55,42 @@ class MainActivity : ComponentActivity() {
 //                Text(text = "Dev")
 //            }
 
-            Row(
+//            Row(
+//                modifier = Modifier
+//                    .width(300.dp)
+//                    .fillMaxHeight(0.6f) // 60% of screen height
+//                    .background(Color.Green),
+//                horizontalArrangement = Arrangement.SpaceAround,
+//                verticalAlignment = Alignment.CenterVertically
+//            ) {
+//                Text(text = "Abora", color = Color.Blue , fontSize = 20.sp)
+//                Text(text = "Android")
+//                Text(text = "Dev")
+//            }
+
+            Column(
                 modifier = Modifier
-                    .width(300.dp)
-                    .fillMaxHeight(0.6f) // 60% of screen height
-                    .background(Color.Green),
-                horizontalArrangement = Arrangement.SpaceAround,
-                verticalAlignment = Alignment.CenterVertically
+                    .background(Color.Green)
+                    .fillMaxHeight(0.5f)
+                    .fillMaxWidth()
+//                    .width(300.dp)
+//                    .requiredWidth(600.dp)
+//                    .padding(top=20.dp , bottom = 30.dp)
+                    .padding(10.dp)
+                    .border(5.dp, Color.Yellow)
+                    .padding(5.dp)
+                    .border(10.dp, Color.Red, shape = CircleShape),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceAround
             ) {
-                Text(text = "Abora", color = Color.Blue , fontSize = 20.sp)
+                Text(
+                    text = "Abora", modifier = Modifier
+                        .offset(20.dp, 20.dp) // like margin without push view away
+                        .clickable { Toast.makeText(this@MainActivity,"hello",Toast.LENGTH_LONG).show() }
+                )
+                Spacer(modifier = Modifier.height(50.dp))
                 Text(text = "Android")
-                Text(text = "Dev")
             }
-
-
         }
     }
 }
